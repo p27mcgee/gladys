6cffeebce2435d4bd5c8054cd90e76b4267288e0
pmcgee <pmcgee@xfi.com>
2018-10-29 03:33:33 -0400

    tweaked goat.bat and log4j.xml for github release

--- goat.bat
+++ goat.bat
@@ -1,5 +1,3 @@
-REM java -javaagent:<path to GladysAgent jar> -Xbootclasspath/a:<path to GladysBootLoaderJar>  -Dlog4j.configurationFile=<path to log4j2.xml for logging configuration> -jar <path to OWASP WebGoat jar> 
+REM java -javaagent:<path to GladysAgent jar> -Xbootclasspath/a:<path to GladysBootLoaderJar> -Dlog4j.configurationFile=<path to log4j2.xml for logging configuration> -jar <path to OWASP WebGoat jar> 
 
-java -javaagent:E:/W_SharedRepo/com/github/p27mcgee/gladys/gladys-agent/1.0.0-alpha3/gladys-agent-1.0.0-alpha3.jar -Xbootclasspath/a:E:/W_SharedRepo/com/github/p27mcgee/gladys/gladys-bootstrap-jar/1.0.0-alpha3/gladys-bootstrap-jar-1.0.0-alpha3.jar -Dlog4j.configurationFile=log4j2.xml -jar webgoat-server-8.0.0.M3.jar 
-
-
+java -javaagent:gladys-agent/target/gladys-agent-1.0.0-alpha3.jar -Xbootclasspath/a:gladys-bootstrap-jar/target/gladys-bootstrap-jar-1.0.0-alpha3.jar -Dlog4j.configurationFile=log4j2.xml -jar webgoat-server-8.0.0.M3.jar
